e26a32a5bda757b56a4ff5eddfe2e6e46c81987e
Bojan Panjevic <panjevicbojan@gmail.com>
2018-09-08 11:10:04 -0500

    Added TimeCapsule contract

---
+++ client/contracts/TimeCapsule.sol
@@ -0,0 +1,67 @@
+pragma solidity ^0.4.23;
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract TimeCapsule is Ownable {
+
+    constructor() public {        
+    }
+
+    struct Message {
+        uint MessageId;
+        address Sender;
+        address Recipient; 
+        string PolicyId;
+        string SigningKey;
+        string VerifyingKey;        
+        string Timestamp;
+        string FileLink;
+    }
+
+    uint messageCount;
+    Message[] allMessages;
+
+    event MessageCreated(uint messageId);
+    event MessageRevoked(uint messageId);
+   
+    mapping (address => uint[]) public userMessages; // Messages that the current user has added
+    mapping (address => uint[]) public messagesForUser;
+
+    function createMessage(address recipient, string policyId, string signingKey, string verifyingKey, string timeStamp, string fileLink)
+     external {
+        var messageId = messageCount++;
+
+        Message memory message = Message({
+            MessageId : messageId,
+            Sender : msg.sender,
+            Recipient : recipient,
+            PolicyId : policyId,
+            SigningKey : signingKey,
+            VerifyingKey : verifyingKey,
+            Timestamp : timeStamp,
+            FileLink : fileLink
+        });
+
+        allMessages[messageId] = message;        
+        userMessages[msg.sender].push(messageId);
+        messagesForUser[recipient].push(messageId);
+
+        emit MessageCreated(messageId);
+    }
+
+    function revokeMessage(uint messageId) external payable{
+        // TODO: Implement
+        // retrieve the message
+        // check if you're the sender of the message and the time hasn't passed
+        
+        emit MessageRevoked(messageId);
+    }
+
+    function getMessagesForUser() public view returns(uint[]) {
+        return messagesForUser[msg.sender];
+    }
+
+    function getMessagesByUser() public view returns(uint[]) {
+        return userMessages[msg.sender];
+    }
+}
--- client/truffle.js
+++ client/truffle.js
@@ -1,18 +1,21 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "";
+
+
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    // ropsten: {
+    //   provider: function() {
+    //     return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/9e9ca8465879458f8a9193f93a35d755")
+    //   },
+    //   network_id: 3,
+    //   gas : 4700000
+    // },
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    }
+  }
 };
